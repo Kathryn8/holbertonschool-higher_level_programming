4b557b091d11d4aa4c8e8454077a17bb2e9ec6c1
Kathryn6368 <6368@holbertonstudents.com>
2023-06-02 23:42:38 -0700

    add logic so code is not executed when imported

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 import sys
-argv = sys.argv
-if len(argv) < 2:
-    print("{} arguments.".format(len(argv) - 1))
-elif len(argv) == 2:
-    print("{} argument:".format(len(argv) - 1))
-else:
-    print("{} arguments:".format(len(argv) - 1))
-for i in range(1, len(argv)):
-    print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    argv = sys.argv
+    argc = len(argv)
+    if argc < 2:
+        print("{} arguments.".format(argc - 1))
+    elif argc == 2:
+        print("{} argument:".format(argc - 1))
+    else:
+        print("{} arguments:".format(argc - 1))
+    for i in range(1, argc):
+        print("{}: {}".format(i, argv[i]))
