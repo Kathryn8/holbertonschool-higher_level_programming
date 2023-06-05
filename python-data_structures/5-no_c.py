1076c25ddfb913ccf5ade381b750da69e5e1c6b9
Kathryn6368 <6368@holbertonstudents.com>
2023-06-04 20:01:18 -0700

    refactor code

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+
+
 def no_c(my_string):
     my_list = []
-    for i in range(0, len(my_string)):
-        if my_string[i] != 'c' and my_string[i] != 'C':
-            my_list.append(my_string[i])
-        new_string = ''.join(my_list)
+    my_list[:0] = my_string
+    new_string = ''
+    for i in range(0, len(my_list)):
+        if my_list[i] != 'c' and my_list[i] != 'C':
+            new_string = new_string + ''.join(my_list[i])
     return new_string
