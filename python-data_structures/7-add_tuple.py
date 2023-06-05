1449e53021ad165bf21d61c698dd055528910860
Kathryn6368 <6368@holbertonstudents.com>
2023-06-05 02:52:40 -0700

    correct tuple notation

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,7 +3,7 @@
 
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) < 1 and len(tuple_b) < 1:
-        return ()
+        return (0, 0)
     if len(tuple_a) < 1:
         return (tuple_b[0], tuple_b[1])
     if len(tuple_b) < 1:
