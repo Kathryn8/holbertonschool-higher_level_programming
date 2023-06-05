dd51ef08c95a6c5e1b6d31ace950d15a29dd3571
Kathryn6368 <6368@holbertonstudents.com>
2023-06-04 19:46:18 -0700

    adjust indentation to change logic

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -4,5 +4,5 @@ def no_c(my_string):
     for i in range(0, len(my_string)):
         if my_string[i] != 'c' and my_string[i] != 'C':
             my_list.append(my_string[i])
-            new_string = ''.join(my_list)
+        new_string = ''.join(my_list)
     return new_string
