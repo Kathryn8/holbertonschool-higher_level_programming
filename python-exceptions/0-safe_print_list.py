52c7d24db31c5d1b6f22fac1c9ad70987dfe7e65
Kathryn6368 <6368@holbertonstudents.com>
2023-06-07 17:38:25 -0700

    add logic to check for zero case and empty list

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,6 +2,10 @@
 
 
 def safe_print_list(my_list=[], x=0):
+    if x < 1:
+        return 0
+    if my_list is None:
+        return 0
     try:
         for i in range(0, x):
             print(my_list[i], end="")
