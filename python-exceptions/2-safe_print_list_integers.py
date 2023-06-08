f08765e45beea8102a71e1a5d5825497056b878a
Kathryn6368 <6368@holbertonstudents.com>
2023-06-07 21:39:57 -0700

    remove zero and empty list logic

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -2,10 +2,6 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-    if x < 1:
-        return 0
-    if my_list is None:
-        return 0
     counter = 0
     for i in range(0, x):
         try:
