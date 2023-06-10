402d3e291b4a788709169dc92c6346fbb9d05b89
Kathryn6368 <6368@holbertonstudents.com>
2023-06-10 03:44:40 -0700

    add checks for edge case of None values in dictionary

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,6 +4,6 @@ def best_score(a_dictionary):
         return None
     key_list = list(a_dictionary.keys())
     values_list = list(a_dictionary.values())
-    max_value = max(values_list)
+    max_value = max([i for i in values_list if i is not None])
     pos = values_list.index(max_value)
     return key_list[pos]
