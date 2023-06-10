ca07e7faa399aae5ed2b297903f871c42419df40
Kathryn6368 <6368@holbertonstudents.com>
2023-06-09 20:31:30 -0700

    best refector yet?

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-def roman_to_int(roman_string):
-    if type(roman_string) is not str or roman_string is None:
-        return 0
+def roman_to_int(s):
     d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    num = 0
-    for i in range(0, len(roman_string)):
-        try:
-            if d.get(roman_string[i]) < d.get(roman_string[i + 1]):
-                x = -1 * d.get(roman_string[i])
-            else:
-                x = d.get(roman_string[i])
-        except IndexError:
-            x = d.get(roman_string[i])
-        finally:
-            num = num + x
-    return num
+    n = 0
+    if type(s) is str and s is not None:
+        for i, c in enumerate(s):
+            try:
+                n = n+(-1*d.get(c) if d.get(c) < d.get(s[i+1]) else (d.get(c)))
+            except IndexError:
+                n = n + d.get(c)
+    return n
