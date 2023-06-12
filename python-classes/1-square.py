e78a4831ffaba089e3769d35da69cc2acb5099ec
Kathryn6368 <6368@holbertonstudents.com>
2023-06-11 21:53:23 -0700

    add file

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+"""Define a class Square."""
+
+
+class Square():
+    """An class called Square. Currently storing nothing"""
+    def __init__(self, size):
+        self.__size = size
