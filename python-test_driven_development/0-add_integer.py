1888bb0fb989e4eedbb639e29fef1248634da372
Kathryn6368 <6368@holbertonstudents.com>
2023-06-14 21:49:51 -0700

    add checks in function for successful cast to int

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -22,7 +22,8 @@ def add_integer(a, b=98):
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
-    if a is not None and b is not None:
-        return int(a) + int(b)
-    else:
-        return 0
+    try:
+        result =  int(a) + int(b)
+    except NameError:
+        result = 0
+    return result
