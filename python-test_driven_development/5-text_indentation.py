e80959c8c6e76c8936d3d5d3c4b8578f83c08a1f
Kathryn6368 <6368@holbertonstudents.com>
2023-06-15 16:14:12 -0700

    edit stringmanipulation for all edges cases

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -5,12 +5,25 @@ Module description"
 """
 
 
+def multisplit(s, delims):
+    """
+    Description of multisplit function
+    """
+    pos = 0
+    for i, c in enumerate(s):
+        if c in delims:
+            yield s[pos:i+1]
+            pos = i + 1
+    yield s[pos:]
+
+
 def text_indentation(text):
     """
     Description of text_indentation function
     """
     if type(text) != str:
         raise TypeError("text must be a string")
-    print(text.replace("? ", "?\n\n")
-          .replace(": ", ":\n\n")
-          .replace(". ", ".\n\n"), end="")
+    result = list(multisplit(text, '.:?'))
+    result = list(map(lambda s: s.strip(), result))
+    result = "\n\n".join(result)
+    print(result, end="")
