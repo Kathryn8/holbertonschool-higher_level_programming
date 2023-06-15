974d427e7967b795b3ca6c94d147e4522455ca56
Kathryn6368 <6368@holbertonstudents.com>
2023-06-15 03:15:01 -0700

    add files

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""
+Module description"
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Description of say_my_name function
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
