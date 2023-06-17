b9576ce7fa8922dc2c29c93f6931f03a0fe0980f
Kathryn6368 <6368@holbertonstudents.com>
2023-06-17 01:23:58 -0700

    format docstrings

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,24 +10,18 @@ class Rectangle:
     Description of the class Rectangle
     """
     def __init__(self, width=0, height=0):
-        """
-        Description of instantiation function
-        """
+        """Description of instantiation function"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """
-        Instance method that returns the private instance variable width
-        """
+        """Instance method that returns the private instance variable width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """
-        Instance method that sets the private instance variable width
-        """
+        """Instance method that sets the private instance variable width"""
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -36,16 +30,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """
-        Instance method that returns the private instance variable width
-        """
+        """Instance method that returns the private instance variable width"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """
-        Instance method that sets the private instance variable width
-        """
+        """Instance method that sets the private instance variable width"""
         if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
