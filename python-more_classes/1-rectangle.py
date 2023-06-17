5aea498f6fad890a8024f4384fb1e5fe214bd0ad
Kathryn6368 <6368@holbertonstudents.com>
2023-06-17 02:05:07 -0700

    test commit

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,48 +1,39 @@
 #!/usr/bin/python3
-
-"""
-Description of the module called 0-rectangle
-"""
+"""Class that defines a rectangle:"""
 
 
 class Rectangle:
-    """
-    Description of the class Rectangle
-    """
+    """A class that define a rectangle"""
+
     def __init__(self, width=0, height=0):
-        """
-        Description of instantiation function
-        Arguments: width, height
-        """
-        self.height = height
+        """Initialize rectangle instance with optional width and height"""
         self.width = width
-
-    @property
-    def height(self):
-        """Instance method that returns the private instance variable width"""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Instance method that sets the private instance variable width"""
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.___height = value
+        self.height = height
 
     @property
     def width(self):
-        """Instance method that returns the private instance variable width"""
+        """Retrieve width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Instance method that sets the private instance variable width"""
-        if type(value) != int:
+        """Set width"""
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
