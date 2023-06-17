8dbc48d319d2401dce6c86825b7a88a08b7bf1a9
Kathryn6368 <6368@holbertonstudents.com>
2023-06-17 02:38:07 -0700

    use isinstance instead of type

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Instance method that sets the private instance variable height"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
@@ -36,7 +36,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Instance method that sets the private instance variable width"""
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
