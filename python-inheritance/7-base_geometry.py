41cefb0ec58d579457ce87a7456d4d59962d015c
Kathryn6368 <6368@holbertonstudents.com>
2023-06-20 16:27:50 -0700

    change type to isintance and fix typo

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -8,7 +8,7 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if isintance(value, int):
+        if isinstance(value, int) is not True:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
