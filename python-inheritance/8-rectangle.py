6a5a2e38711e2c80fd9e7110ff02a943c1665178
Kathryn6368 <6368@holbertonstudents.com>
2023-06-21 16:09:39 -0700

    separate rect and basegeom classes

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,18 +1,6 @@
 #!/usr/bin/python3
 """Module that contains the class BaseGeometry"""
-
-
-class BaseGeometry:
-    """A class called BaseGeometry"""
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-        return value
+Rectangle = __import__('8-rectangle').Rectangle
 
 
 class Rectangle(BaseGeometry):
