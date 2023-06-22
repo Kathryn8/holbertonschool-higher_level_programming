ca3e64d1d866762807af83c2b032830e43f79ccb
Kathryn6368 <6368@holbertonstudents.com>
2023-06-22 15:55:34 -0700

    add file close statement

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,6 +5,7 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Writes python object to text file in json format"""
-    json_str = json.dumps(my_obj)
     with open(filename, 'w', encoding="utf=8") as open_file:
+        json_str = json.dumps(my_obj)
         num_char = open_file.write(json_str)
+    open_file.close()
