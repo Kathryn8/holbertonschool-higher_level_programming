9581a352b0986b9a651916062e12ed5d89fb588a
Kathryn6368 <6368@holbertonstudents.com>
2023-06-23 17:17:56 -0700

    its working!

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -10,27 +10,13 @@ def pascal_triangle(n):
     for idx, row in enumerate(matrix):
         for i in range(idx):
             row.append(1)
-        """
-        if idx % 2 != 0: #odd idx
-            row.append(1)
-        if idx % 2 == 0 and idx > 0: #even idx
-            row.append(1)"""
     for idx, row in enumerate(matrix):
         for idxj, col in enumerate(row):
-            #print("col is: {} / idx: {}".format(col, idx))
-#            if idx > idxj:
-            if idx > 0:
+            if idx > 1 and idxj > 0:
                 try:
-                    col = matrix[idx-1][idxj-1] + matrix[idx-1][idxj]
+                    row[idxj] = matrix[idx-1][idxj-1] + matrix[idx-1][idxj]
                 except IndexError:
-                    col = matrix[idx-1][idxj-1] + 0
+                    row[idxj] = matrix[idx-1][idxj-1] + 0
                 except IndexError:
-                    col = matrix[idx-1][idxj] + 0
-                    """
-                    if idx > 1:
-                    row[1] = matrix[idx-1][0] + matrix[idx-1][1]
-                    if idx > 2:
-                    row[2] = matrix[idx-1][1] + matrix[idx-1][2]
-                    if idx > 3:
-                    row[3] = matrix[idx-1][2] + matrix[idx-1][3]"""
+                    row[idxj] = matrix[idx-1][idxj] + 0
     return matrix
