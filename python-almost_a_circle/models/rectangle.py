b28e123532ab63c840be929faacb553f4590f168
Kathryn6368 <6368@holbertonstudents.com>
2023-06-26 03:12:52 -0700

    update for task 7 Display #1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -78,8 +78,9 @@ class Rectangle(Base):
 
     def display(self):
         """ prints in stdout the Rectangle instance with the character #"""
+        print("\n" * self.__y, end="")
         for row in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
 
     def __str__(self):
         coordinates = f"{self.__x}/{self.__y}"
