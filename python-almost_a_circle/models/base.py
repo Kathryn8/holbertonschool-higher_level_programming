b1eda6ba8c0f7add48bc9fa036cc7a50052f2cd2
Kathryn6368 <6368@holbertonstudents.com>
2023-06-28 07:21:43 -0700

    update for task 18

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -33,8 +33,20 @@ class Base:
         with open(filename, 'w', encoding="utf-8") as new_file:
             new_file.write(json_string)
 
+    @staticmethod
     def from_json_string(json_string):
         """returns the list of the JSON string representation"""
         if json_string is None or json_string == []:
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """returns an instance with all attributes already set"""
+        if dictionary != {}:
+            if cls.__name__ == 'Rectangle':
+                dummy = cls(2, 3)
+            else:
+                dummy = cls(2)
+        dummy.update(**dictionary)
+        return dummy
