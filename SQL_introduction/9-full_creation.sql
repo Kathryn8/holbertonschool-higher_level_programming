276f6f20329882cf36c3a4b4069caeb836962bad
Kathryn6368 <6368@holbertonstudents.com>
2023-07-24 23:36:45 -0700

    string to int

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -6,7 +6,7 @@ score INT
 );
 INSERT INTO second_table(id, name, score)
 VALUES
-	('1', 'John', '1'),
-	('2', 'Alex', '3'),
-	('3', 'Bob', '14'),
-	('4', 'George','8');
+	(1, 'John', 1),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
