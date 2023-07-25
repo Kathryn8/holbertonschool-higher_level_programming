f3c624f666f6663165a0b209c396a9cf8f6429cb
Kathryn6368 <6368@holbertonstudents.com>
2023-07-24 22:58:16 -0700

    add file

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,3 @@
--- script that lists all rows of the table first_table from the database
-INSERT INTO first_table(column_1, column_2) VALUES ('89', 'Best School');
+-- inserts a new row in the table first_table
+INSERT INTO first_table(id, name)
+VALUES ('89', 'Best School');
