85b4dd5ab7875d1beeb43fc4217144a13842196d
Kathryn6368 <6368@holbertonstudents.com>
2023-07-24 18:48:42 -0700

    add file

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- Write a script that creates the database hbtn_0c_0 in your MySQL server.
 -- Create a database and list databases to standard output
-CREATE database hbtn_0c_0;
+CREATE DATABASE hbtn_0c_0;
