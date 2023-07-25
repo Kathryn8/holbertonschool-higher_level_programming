269b2ae748534bdeeb9dc064d8aade1c6b61608c
Kathryn6368 <6368@holbertonstudents.com>
2023-07-24 19:58:40 -0700

    add qulifier

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- Write a script that deletes the database hbtn_0c_0 in your MySQL server.
 -- Delete the database hbtn_0c_0 in your MySQL server.
-DROP DATABASE hbtn_0c_0;
+DROP database IF EXISTS hbtn_0c_0;
