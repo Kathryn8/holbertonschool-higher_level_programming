b1bcdff20db2dd9221f7445db2fbc86a95cde338
Kathryn6368 <6368@holbertonstudents.com>
2023-07-31 16:15:45 -0700

    include NOT NULL on name column

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -14,7 +14,7 @@ class State(Base):
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(128))
+    name = Column(String(128), nullable=False)
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://root:root@localhost:3306/hb\
