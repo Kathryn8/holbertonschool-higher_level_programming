3f36edeea8122c77648b994e6d01f8dd274ab261
Kathryn6368 <6368@holbertonstudents.com>
2023-07-30 21:15:12 -0700

    add file

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+script that takes in an argument and displays all values in the states
+table of hbtn_0e_0_usa where name matches the argument
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    v = sys.argv
+    db = MySQLdb.connect(host="localhost", user=v[1],
+                         passwd=v[2], db=v[3])
+    cur = db.cursor()
+    q = "SELECT * FROM states ORDER BY id;"
+    if v[4]:
+        q = "SELECT * FROM states WHERE name='{}' ORDER BY id;".format(v[4])
+    cur.execute(q)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
