9aaaf1633ecc639228544a6a4e32575e2c244693
Kathryn6368 <6368@holbertonstudents.com>
2023-07-30 22:31:15 -0700

    include the BINARY keywork in sql query

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", user=v[1],
                          passwd=v[2], db=v[3])
     cur = db.cursor()
-    q = "SELECT * FROM states WHERE name='{}' ORDER BY id;".format(v[4])
+    q = "SELECT * FROM states ORDER BY id;"
+    if v[4]:
+        q = "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id;"\
+            .format(v[4])
     cur.execute(q)
     rows = cur.fetchall()
     for row in rows:
