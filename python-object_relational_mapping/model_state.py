fb1c7652ca868a325e137d0570ba99bfe81ff752
Kathryn6368 <6368@holbertonstudents.com>
2023-07-31 16:02:48 -0700

    add file

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+"""python file that contains the class definition of a State"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+eng = create_engine("mysql+mysqldb://root:root@localhost:3306/hbtn_0e_6_usa")
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+
+
+Base.metadata.create_all(eng)
