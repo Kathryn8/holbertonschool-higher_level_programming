f429976855bc082d68c0057eb370ffbf55028dc8
Kathryn6368 <6368@holbertonstudents.com>
2023-07-31 20:59:31 -0700

    add file

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Python file that contains the class definition of a State
+"""
+
+from sqlalchemy import create_engine
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    pw = sys.argv[2]
+    db = sys.argv[3]
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(user, pw, db)
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    session.query(State).\
+        filter(State.id == 2).\
+        update({State.name: 'New Mexico'})
+    session.commit()
+    session.close()
