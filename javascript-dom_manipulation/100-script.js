b76a1421006f628053e7e35628c6a756dc4c5635
Kathryn Kelly <kathryn@Kathryns-Air.lan>
2023-09-13 06:15:36 +1000

    add file

---
+++ javascript-dom_manipulation/100-script.js
@@ -0,0 +1,22 @@
+const add = document.body.querySelector("#add_item");
+const remove = document.body.querySelector("#remove_item");
+const clear = document.body.querySelector("#clear_list");
+const parent = document.body.querySelector(".my_list");
+
+
+add.addEventListener("click", () => {
+  let li = parent.appendChild(document.createElement("li"));
+  li.textContent = "Item";
+})
+
+remove.addEventListener("click", () => {
+  if (parent.hasChildNodes()) {
+    parent.removeChild(document.body.querySelector("li"));
+  }
+})
+
+clear.addEventListener("click", () => {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+})
