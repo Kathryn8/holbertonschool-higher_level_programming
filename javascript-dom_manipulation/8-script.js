e92bc5bcc3f0ce1a7bebd74b88f3c97e6f36c854
Kathryn Kelly <kathryn@Kathryns-Air.lan>
2023-09-12 21:14:34 +1000

    add file

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,14 @@
+hello = document.body.querySelector("#hello");
+
+async function fetchHello() {
+  response = await fetch("https://hellosalut.stefanbohacek.dev/?lang=fr");
+  data = await response.json();
+  return data
+}
+
+async function displayHello() {
+  data = await fetchHello()
+  hello.textContent = data.hello
+}
+
+displayHello()
