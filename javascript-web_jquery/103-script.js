216c49fb8001ba9efb408f4879658f7333037cb6
Kathryn6368 <6368@holbertonstudents.com>
2023-09-18 19:58:31 +1000

    add file

---
+++ javascript-web_jquery/103-script.js
@@ -0,0 +1,22 @@
+const lang_list = ['ar', 'az', 'be', 'bg', 'bn', 'bs', 'cs', 'da', 'de', 'dz', 'el', 'en', 'en - gb', 'en - us', 'es', 'et', 'fa', 'fi', 'fil', 'fr', 'he', 'hi', 'hr', 'hu', 'hy', 'id', 'is', 'it', 'ja', 'ka', 'kk', 'km', 'ko', 'lb', 'lo', 'lt', 'lv', 'mk', 'mn', 'ms', 'my', 'ne', 'no', 'pl', 'pt', 'ro', 'ru', 'sk', 'sl', 'sq', 'sr', 'sv', 'sw', 'th', 'tk', 'uk', 'vi', 'zh'];
+
+function translate() {
+  const lang = $("#language_code").val()
+  console.log(lang)
+  if (lang_list.includes(lang)) {
+    $.get(`https://hellosalut.stefanbohacek.dev/?lang=${lang}`, function (data) {
+      $('#hello').text(data.hello);
+    })
+  } else if (lang) {
+    $('#hello').text("Sorry - not a supported language");
+  }
+}
+
+$(document).ready(function () {
+  $("#btn_translate").click(translate)
+  $('#btn_translate').keypress((e) => {
+    if (e.which === 13) {
+      translate;
+    }
+  })
+})
